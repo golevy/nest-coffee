3e7bf5950aac748b7d959047c3886007f4d5ff9a
Levy Lv <golvwei@gmail.com>
2024-03-20 01:02:18 +0800

    feat: Use route parameters (#2)

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
+// Defines a controller with a base route of '/coffees'
 @Controller('coffees')
-export class CoffeesController {}
+export class CoffeesController {
+  // Handles GET requests to '/coffees', returning all coffees
+  @Get()
+  findAll() {
+    return 'This action returns all coffees';
+  }
+
+  // Handles GET requests to '/coffees/:id', fetching a specific coffee by its id
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} coffee`;
+  }
+}
