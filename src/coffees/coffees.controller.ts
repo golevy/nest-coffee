4a76447584dbf0c5f622a5ded8f9e65b948705b2
Levy Lv <golvwei@gmail.com>
2024-03-23 01:51:07 +0800

    feat: Response status codes (#4)

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,13 +1,27 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 
 // Defines a controller with a base route of '/coffees'
 @Controller('coffees')
 export class CoffeesController {
   // Handles GET requests to '/coffees', returning all coffees
   @Get()
-  findAll() {
-    return 'This action returns all coffees';
+  // Below is an example of using express response object to send a response
+  // But It is recommended to use the NestJS standard approach when dealing with responses whenever possible, because it provides a more consistent and predictable behavior across the application.
+  findAll(@Res() response) {
+    response.status(200).send('This action returns all coffees');
   }
+  // findAll() {
+  //   return 'This action returns all coffees';
+  // }
 
   // Handles GET requests to '/coffees/:id', fetching a specific coffee by its id
   @Get(':id')
@@ -16,6 +30,7 @@ export class CoffeesController {
   }
 
   @Post()
+  @HttpCode(HttpStatus.GONE)
   create(@Body() body) {
     return body;
   }
