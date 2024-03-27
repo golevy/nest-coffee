3b8af215576835764098b579afc96e4d668c6b53
Levy Lv <golvwei@gmail.com>
2024-03-27 23:49:33 +0800

    feat: Validate input data with data transfer objects (#11)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); // means that all incoming requests will be validated by the ValidationPipe
   await app.listen(8080);
 }
 bootstrap();
