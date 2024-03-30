06d538cf17ee6fd132cc5e2ade293b4e6d17eb41
Levy Lv <golvwei@gmail.com>
2024-03-30 23:38:03 +0800

    feat: Creating a TypeORM entity (#16)

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity() // SQL table === 'coffee'
 export class Coffee {
+  @PrimaryGeneratedColumn() // SQL column === 'id'
   id: number;
+
+  @Column() // SQL column === 'name'
   name: string;
+
+  @Column() // SQL column === 'brand'
   brand: string;
+
+  @Column('json', { nullable: true }) // SQL column === 'flavors'
   flavors: string[];
 }
