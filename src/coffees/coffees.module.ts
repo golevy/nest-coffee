9e765c74298d12002da78d75fb98243c2ea450b5
Levy Lv <golvwei@gmail.com>
2024-04-06 21:56:04 +0800

    feat: Class providers (#28)

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -7,11 +7,22 @@ import { Flavor } from './entities/flavor.entity';
 import { Event } from 'src/events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
 
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])], // 👈 Adding Entities here to the imports
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
+    },
     {
       // Token to inject coffee brands array across the application
       provide: COFFEE_BRANDS,
