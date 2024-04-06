04a5e68f8ee2690d95358b6e33bc29d413a39ef3
Levy <golvwei@gmail.com>
2024-04-06 20:58:44 +0800

    feat: Value based providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,10 +6,22 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from 'src/events/entities/event.entity';
 
+class MockCoffeeService {}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])], // 👈 Adding Entities here to the imports
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [
+    {
+      provide: CoffeesService, // 👈 This is the token that can be used to inject the dependency
+      useValue: new MockCoffeeService(), // 👈 Here we provide a mock implementation for CoffeesService
+      // useValue allows us to provide a specific value as a service. In this case,
+      // instead of the actual CoffeesService, we're using a mock service.
+      // This is useful for testing, as it allows us to replace a real service
+      // with a mock implementation, ensuring our tests are not affected by the
+      // actual logic of the CoffeesService.
+    },
+  ],
   exports: [CoffeesService], // 👈 Making the provider available to other modules
 })
 export class CoffeesModule {}
