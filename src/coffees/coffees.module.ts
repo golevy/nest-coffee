09316ca399843536d941e38d3385fe1e963c54b9
Levy Lv <golvwei@gmail.com>
2024-04-09 21:37:06 +0800

    feat: Use config service (#36)

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,30 +1,16 @@
-import { Module, Scope } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from 'src/events/entities/event.entity';
-import { COFFEE_BRANDS } from './coffees.constants';
-import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])], // 👈 Adding Entities here to the imports
+  imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event]), ConfigModule],
   controllers: [CoffeesController],
-  providers: [
-    CoffeesService,
-    {
-      // Token to inject coffee brands array across the application
-      provide: COFFEE_BRANDS,
-      useFactory: async (coffeeConnection: DataSource): Promise<string[]> => {
-        const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe']);
-        console.log('[!] Async factory');
-        return coffeeBrands;
-      },
-      scope: Scope.TRANSIENT,
-      inject: [DataSource],
-    },
-  ],
+  providers: [CoffeesService],
   exports: [CoffeesService], // 👈 Making the provider available to other modules
 })
 export class CoffeesModule {}
