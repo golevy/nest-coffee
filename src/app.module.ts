97e0924b10287c5544227d414da504c5f03cb227
Levy Lv <golvwei@gmail.com>
2024-04-09 22:26:06 +0800

    feat: Asynchronously configure dynamic modules (#39)

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,20 +10,22 @@ import appConfig from './config/app.config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true, // models will be loaded automatically (not recommended for production)
+        synchronize: true, // Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
+      }),
+    }),
     ConfigModule.forRoot({
       load: [appConfig],
     }),
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true, // models will be loaded automatically (not recommended for production)
-      synchronize: true, // Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
-    }),
     CoffeeRatingModule,
     DatabaseModule,
   ],
