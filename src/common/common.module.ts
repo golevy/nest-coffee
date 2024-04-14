2dfe24bc552099c434e89b490789e93d224b5db2
Levy Lv <golvwei@gmail.com>
2024-04-14 19:16:03 +0800

    feat: Bonus add request logging with middleware (#47)

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { ApiKeyGuard } from './guards/api-key.guard';
+import { LoggingMiddleware } from './middleware/logging.middleware';
 
 @Module({
   imports: [ConfigModule],
@@ -12,4 +13,11 @@ import { ApiKeyGuard } from './guards/api-key.guard';
     },
   ],
 })
-export class CommonModule {}
+export class CommonModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*'); // Apply the middleware to all routes
+    // consumer.apply(LoggingMiddleware).forRoutes('coffees'); // Apply the middleware to the coffees route
+    // consumer.apply(LoggingMiddleware).forRoutes({ path: 'coffees', method: RequestMethod.GET }); // Apply the middleware to the coffees route for GET requests
+    // consumer.apply(LoggingMiddleware).exclude('coffees').forRoutes('*'); // Apply the middleware to all routes except the coffees route
+  }
+}
