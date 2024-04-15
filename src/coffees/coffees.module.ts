10cb56e5e026c67af52cb700f11d62bcda14b47f
Levy <golvwei@gmail.com>
2024-04-15 23:59:05 +0800

    Add relative path

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,7 +5,7 @@ import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
-import { Event } from 'src/events/entities/event.entity';
+import { Event } from '../events/entities/event.entity';
 import coffeesConfig from './config/coffees.config';
 
 @Module({
