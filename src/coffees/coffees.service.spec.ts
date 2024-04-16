7b32c0f1ef841ef04f84795f5a6544a37b90b1c7
Levy <golvwei@gmail.com>
2024-04-16 23:04:21 +0800

    feat: Add unit test

--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,13 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoffeesService } from './coffees.service';
-import { DataSource } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import coffeesConfig from './config/coffees.config';
+import { NotFoundException } from '@nestjs/common';
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+const createMockRepository = <T = any>(): MockRepository<T> => ({
+  findOne: jest.fn(),
+  create: jest.fn(),
+});
 
 describe('CoffeesService', () => {
   let service: CoffeesService;
+  let coffeeRepository: MockRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -19,11 +27,11 @@ describe('CoffeesService', () => {
         },
         {
           provide: getRepositoryToken(Coffee),
-          useValue: {},
+          useValue: createMockRepository(),
         },
         {
           provide: getRepositoryToken(Flavor),
-          useValue: {},
+          useValue: createMockRepository(),
         },
         {
           provide: coffeesConfig.KEY,
@@ -32,11 +40,38 @@ describe('CoffeesService', () => {
       ],
     }).compile();
 
-    // service = module.get<CoffeesService>(CoffeesService); // For default-scoped services, use module.get() to retrieve the service instance synchronously.
-    service = await module.resolve<CoffeesService>(CoffeesService); // For request-scoped services, use module.resolve() to retrieve the service instance asynchronously.
+    service = module.get<CoffeesService>(CoffeesService); // For default-scoped services, use module.get() to retrieve the service instance synchronously.
+    // service = await module.resolve<CoffeesService>(CoffeesService); // For request-scoped services, use module.resolve() to retrieve the service instance asynchronously.
+    coffeeRepository = module.get<MockRepository>(getRepositoryToken(Coffee));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    describe('when coffee with ID exists', () => {
+      it('should return the coffee object', async () => {
+        const coffeeId = 1;
+        const expectedCoffee = {};
+
+        coffeeRepository.findOne.mockReturnValue(expectedCoffee);
+        const coffee = await service.findOne(coffeeId);
+        expect(coffee).toEqual(expectedCoffee);
+      });
+    });
+    describe('otherwise', () => {
+      it('should throw the "NotFoundException"', async () => {
+        const coffeeId = 1;
+        coffeeRepository.findOne.mockReturnValue(undefined);
+
+        try {
+          await service.findOne(coffeeId);
+        } catch (err) {
+          expect(err).toBeInstanceOf(NotFoundException);
+          expect(err.message).toEqual(`Coffee #${coffeeId} not found`);
+        }
+      });
+    });
   });
 });
